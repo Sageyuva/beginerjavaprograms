4c2b754c045bfc99445e43308d18c62e373f9679
Sageyuva <yuvarajdhammure@gmail.com>
2022-09-27 12:07:40 +0530

    class commit

--- src/practice.java
+++ src/practice.java
@@ -6,12 +6,13 @@ public class practice {
         System.out.println("enter the size");
         int sum=0;
         int size = sc.nextInt();
+        System.out.println("enter the "+size+" values");
          int[] a = new int[size];
          for (int i =0 ; i < size; i++){
              a[i]=sc.nextInt();
         }
-         for (int i =0 ; i <=size; i++){
-             sum= sum+i;
+         for (int i =0 ; i <size; i++){
+             sum= sum+a[i];
              System.out.println(sum);
          }
     }
